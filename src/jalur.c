0c2b261d5c969709e1121933db3cd6161b30709c
Azkha Nazzala <113228900+alatteee@users.noreply.github.com>
2025-02-24 09:17:11 +0700

    Update jalur.c

--- src/jalur.c
+++ src/jalur.c
@@ -1,16 +1,14 @@
 #include "../include/jalur.h"
 #include <SDL3/SDL.h>
+#include "../include/config.h"
 
 // Fungsi untuk menggambar jalur
 void drawLanes(SDL_Renderer* renderer, int screenWidth) {
-    int laneWidth = screenWidth / 3;  // Membagi layar menjadi 3 kolom
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // Warna putih
-
-    int i;
-    for (i = 1; i < 3; i++) {
-        // Mengganti SDL_RenderDrawLine dengan SDL_RenderDrawLineF untuk kompatibilitas dengan SDL3
-        SDL_RenderDrawLinesF(renderer, i * laneWidth, 0, i * laneWidth, 600);  // Menggambar garis vertikal
-    }
-
-    SDL_RenderPresent(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);  // White color
+    
+    // Left lane
+    SDL_RenderLine(renderer, screenWidth/3, 0, screenWidth/3, SCREEN_HEIGHT);
+    
+    // Right lane
+    SDL_RenderLine(renderer, (2*screenWidth)/3, 0, (2*screenWidth)/3, SCREEN_HEIGHT);
 }
