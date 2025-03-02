8a7bd4f11eaf90e3b4e654a9023153e93ebc3a06
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-03-03 05:40:55 +0700

    Add files via upload
    file baru untuk header menu mengganti  yang sebelumnya dan sudah di tambahkan program

---
+++ include/menu.h
@@ -0,0 +1,13 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_ttf.h>
+
+// Deklarasi fungsi untuk inisialisasi dan menampilkan menu
+void initMenu(TTF_Font *font);
+void renderMenu(SDL_Renderer *renderer, int selected);
+int handleMenuInput(SDL_Event *event, int *selected);
+void cleanupMenu();
+
+#endif // MENU_H
