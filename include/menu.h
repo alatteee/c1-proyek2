307e91fd1c32185806bbe430c96eb37604fbe1d5
Ersya Hasby Satria <hasbysatria54@gmail.com>
2025-03-03 09:31:22 +0700

    update menu header

---
+++ include/menu.h
@@ -0,0 +1,10 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_ttf.h>
+
+void renderMenu(SDL_Renderer *renderer, TTF_Font *font, int selected);
+int handleMenuInput(SDL_Event *event, int *selected, int menuCount, int (*menuActions[])());
+
+#endif
