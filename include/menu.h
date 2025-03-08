dd140c73f31110a91b6944a294271aef726b2716
Yazid Alrasyid <yazidalrasyid145@gmail.com>
2025-03-09 04:48:16 +0700

    Menambahkan konstanta konfigurasi menu untuk manajemen tata letak yang lebih baik

--- include/menu.h
+++ include/menu.h
@@ -4,6 +4,11 @@
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
+// Menu configuration constants
+#define MENU_ITEM_SPACING 40
+#define MENU_START_Y_POS (SCREEN_HEIGHT/2)
+
+// Function prototypes
 void renderMenu(SDL_Renderer *renderer, TTF_Font *font, int selected);
 int handleMenuInput(SDL_Event *event, int *selected, int menuCount, int (*menuActions[])());
 
